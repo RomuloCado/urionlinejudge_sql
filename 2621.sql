fffa6026c67a5bd84cc2a608cc6c7c03ee5d84b1
Rômulo Cadó Dorneles <79316323+RomuloCado@users.noreply.github.com>
2021-04-19 13:53:12 -0300

    Create 2621.sql

---
+++ 2621.sql
@@ -0,0 +1,6 @@
+/* Exercício 2621 */
+
+SELECT products.name FROM products INNER JOIN providers
+ON products.id_providers = providers.id
+WHERE products.amount BETWEEN 10 AND 20
+AND providers.name LIKE 'P%';
